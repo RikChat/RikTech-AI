6b935f18fc62a6d4f0ab4bc1bb13fb3152486752
Thoriq Ichsani <108530206+RikChat@users.noreply.github.com>
2025-11-16 13:11:00 +0700

    Add files via upload

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,1 +1,13 @@
-y
+import Canvas from '../../components/Canvas';
+import PropertiesPanel from '../../components/PropertiesPanel';
+import ChatPreview from '../../components/ChatPreview';
+
+export default function BuilderPage() {
+  return (
+    <div className="flex h-screen">
+      <Canvas />
+      <PropertiesPanel />
+      <ChatPreview />
+    </div>
+  );
+}
